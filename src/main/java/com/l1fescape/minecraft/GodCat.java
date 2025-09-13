03cc33106f75a3370f2bc9b15d44ae3ab3cf68a2
L1fescape <akennedy2292@gmail.com>
2025-09-12 23:04:34 -0400

    port to forge

--- src/main/java/com/l1fescape/minecraft/GodCat.java
+++ src/main/java/com/l1fescape/minecraft/GodCat.java
@@ -1,11 +1,68 @@
 package com.l1fescape.minecraft;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
 
-public class GodCat extends JavaPlugin {
-  @Override
-  public void onEnable() {
-    getServer().getPluginManager().registerEvents(new CatGodMode(), this);
-  }
+@Mod(GodCat.MODID)
+public class GodCat
+{
+    public static final String MODID = "godcat";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public GodCat(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Bus.FORGE)
+    public static class CatHitEvent
+    {
+        @SubscribeEvent
+        public static void onCatHit(LivingHurtEvent event)
+        {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT ENTITY TAKING DMG >> {}", event.getEntity().getName().getString());
+            LOGGER.info("MINECRAFT ENTITY GIVING DMG >> {}", event.getSource().getEntity().getName().getString());
+            // Fixed string comparison
+            if (event.getEntity().getName().getString().equals("Cat")) {
+                event.setCanceled(true);
+                event.getEntity().setHealth(event.getEntity().getMaxHealth());
+                LOGGER.info("Cat hit event blocked");
+            }
+        }
+    }
 }
